e35b59ea7daf65d4a6b92de7db82fc66f5edf59d
Abhisht51 <abhisht51@gmail.com>
2025-06-30 00:34:16 +0530

    Configure Netlify deployment with proper adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: netlify()
 });
